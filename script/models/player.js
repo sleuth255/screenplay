1dda68b62b7902164279807fd145b7dc8aea8551
Hendrik <hennieh@gmail.com>
2016-12-06 20:37:43 +0200

    Added  pause play on paly area

--- script/models/player.js
+++ script/models/player.js
@@ -157,7 +157,24 @@ Player.prototype.load = function(data, settings) {
 			});
 						
 			console.log("Play Paused - " + time + " : " + ticks);
-		};
+		}; 
+		
+		// Event listener for the playerRegion
+		playerRegion.addEventListener("click", function() {
+			if (video.paused == true) {
+				// Play the video
+				video.play();
+
+				// Update the button text to 'Pause'
+				playButton.innerHTML = "Pause";
+			} else {
+				// Pause the video
+				video.pause();
+
+				// Update the button text to 'Play '
+				playButton.innerHTML = "Play";
+			}
+		});
 		
 		// Event listener for the play/pause button
 		playButton.addEventListener("click", function() {
